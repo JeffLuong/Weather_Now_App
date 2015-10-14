31c2b1bd2b5445fd8e1138fe2b6973068a206ddd
Jeffrey Luong <jjck1988@gmail.com>
2015-10-14 11:29:25 -0400

    more env var testing

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express        = require('express'),
-    server         = express(),
-    ejs            = require('ejs'),
-    expressLayouts = require('express-ejs-layouts'),
-    morgan         = require('morgan'),
-    PORT           = process.env.PORT || 3000,
-    openWeatherKey = process.env.OPEN_WEATHER_KEY;
+var express          = require('express'),
+    server           = express(),
+    ejs              = require('ejs'),
+    expressLayouts   = require('express-ejs-layouts'),
+    morgan           = require('morgan'),
+    PORT             = process.env.PORT || 3000,
+    OPEN_WEATHER_KEY = process.env.OPEN_WEATHER_KEY;
 
-    console.log(openWeatherKey);
+    // console.log(OPEN_WEATHER_KEY);
 
 server.set('views', './views');
 server.set('view engine', 'ejs');
@@ -14,7 +14,7 @@ server.set('view engine', 'ejs');
 server.use(morgan('short'));
 server.use(express.static('./public'));
 server.get('/', function(req, res) {
-  console.log("rendered.");
+  console.log("THIS IS HAPPENINGGGGGG", OPEN_WEATHER_KEY);
   res.render('layout.ejs');
 });
 
